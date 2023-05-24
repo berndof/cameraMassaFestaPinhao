909b20ca1feaac00f493290d25ac3592d7812cca
berndof <bernasoestudas@gmail.com>
2023-05-24 04:53:54 -0300

    eaeaaa

--- app.py
+++ app.py
@@ -1,119 +1,128 @@
-import cv2
 import pygame
 import pygame.camera
-import numpy as np
-import sys 
+
+import os
+import time
+import datetime
+import sys
 
 from PIL import Image
-import os
-import datetime
+#import cv2
 
-screen_res = (1280, 720)
-
-# Pygame
 pygame.init()
 pygame.camera.init()
-screen = pygame.display.set_mode((screen_res[0],screen_res[1]), pygame.FULLSCREEN)
-clock = pygame.time.Clock()
-######################################
 
-######################################
-# Camera
-#cam = cv2.VideoCapture(0)
-#cam.set(cv2.CAP_PROP_FRAME_WIDTH, 913)
-#cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 512)
-cam = pygame.camera.Camera(pygame.camera.list_cameras()[0], (913, 512))
+pygame.camera.list_cameras()
 
-######################################
-# Carregando overlays 
-cam_overlay = pygame.image.load("assets//overlay.png").convert_alpha()
-cam_overlay = pygame.transform.scale(cam_overlay, screen_res)
+for i in range(len(pygame.camera.list_cameras())):
+    camera = pygame.camera.Camera(pygame.camera.list_cameras()[i], (800, 600))
+    try:
+        camera.start()  # Tenta iniciar a camera
+    except pygame.error:
+        print("Failed to connect")
+        # Se não startar, vai pra proxima camera da lista
+        continue
+    print(f"Connected to camera {i}")
+    break  # Se startar quebra o loop 
 
-loadscreen = pygame.image.load('assets//loadscreen.png')
+
+#Define a resolução da camera
+camera_res = (913, 512)
+    
+screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN) # Inicia uma janela em tela cheia
+screen_res = screen.get_size() # Pega a resolução da janela = resolução máxima do monitor
+
+cam_overlay = pygame.image.load("assets//overlay.png").convert_alpha() # carrega o overlay da pasta overlay
+cam_overlay = pygame.transform.scale(cam_overlay, screen_res)# redimennsiona para o tamanho da tela
+
+loadscreen = pygame.image.load("assets//loadscreen.png").convert() # carrega o overlay da pasta overlay
 loadscreen = pygame.transform.scale(loadscreen, screen_res)
-######################################
 
-######################################
-def editImage(filename, screen_res = screen_res):
 
-    imagem = Image.open(os.path.join("temp", filename))
+def editImage(imagem, filename):
+    save_path = "Fotos Massa//" # caminho para salvar
+
+    imagem = Image.open(imagem) # carrega a imagem
     
-    overlay = Image.open('assets//overlay2.png')
-    overlay = overlay.resize(imagem.size)
+    #carrega o ovelay aplicado na foto e redimensiona
+    overlay = Image.open("assets//overlay2.png") 
+    overlay = overlay.resize((imagem.size))
     
-    imagem.paste(overlay, (0, 0), overlay)
-    imagem.save("Fotos Massa//" + filename)
+    #cola a sobreposição e salva
+    imagem.paste(overlay, (0,0), overlay)
+    imagem.save(save_path + filename)
     
+    
+    #apaga a foto temporaria
     os.remove(os.path.join("temp", filename))
     
-    imagem_py = pygame.image.load(os.path.join("Fotos Massa", filename)).convert()
-    imagem_py = pygame.transform.scale(imagem_py, screen_res)
-
-    return imagem_py, True
-
-def frameProcess(frame, screen_res = screen_res):
-
-    #frame = cv2.resize(frame, screen_res, interpolation=cv2.INTER_AREA)
-    #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    #frame = np.rot90(frame)
-    #frame = pygame.surfarray.make_surface(frame)
-    #frame = pygame.transform.flip(frame, True, False)
-
-    frame = pygame.transform.scale(frame, screen_res)
-    screen.blit(frame, (0,0))
-    screen.blit(cam_overlay, (0, 0))
-    pygame.display.flip()
-
-def checkClick():
-    for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                key = "space"
-            elif event.key == pygame.K_ESCAPE:
-                key = "esc"
-            return key
-
-frame = cam.get_image()
+    imagem = pygame.image.load(save_path + filename)
+    
+    return imagem, True
 
 
 while True:
 
-    frameProcess(frame)
+    capture = camera.get_image()
+    
+    scaled_capture = pygame.transform.scale(capture, screen_res)
+    
+    screen.blit(scaled_capture, (0,0))
+    screen.blit(cam_overlay, (0,0))
+    
+    pygame.display.flip()
+    
+    for event in pygame.event.get(): # checa se acontecer o evento 
+        if event.type == pygame.KEYDOWN: # se apressionar espaço 
+            if event.key == pygame.K_SPACE:
+                screen.blit(loadscreen, (0,0))
+                pygame.display.flip()
+                
+                camera.stop()
+                camera = None    
+                camera = pygame.camera.Camera(pygame.camera.list_cameras()[0], (1280, 720))
+                camera.start()
+                
+                capture = camera.get_image()
+                filename = datetime.datetime.now().strftime("%d-%m-%Y_%H-%M-%S Massa-festa-pinhao.png" )
+                pygame.image.save(capture , os.path.join("temp", filename))
 
-    clock.tick(30)
-        
-    frame = cam.get_image()
+                start_time = pygame.time.get_ticks()
+                # Define o tempo da animação
+                animation_time = 150
+                while pygame.time.get_ticks() - start_time < animation_time:
+                    # Alterna entre preto e branco
+                    if pygame.time.get_ticks() % 100 < 50:
+                        screen.fill((255, 255, 255))
+                        pygame.display.flip()
+                    else:
+                        screen.fill((0, 0, 0))
+                        pygame.display.flip()
 
-    if checkClick() == "space":
-        
-        filename = datetime.datetime.now().strftime("%d-%m-%Y_%H-%M-%S Massa-festa-pinhao.png")
+                camera.stop()
+                camera = None
+                camera = pygame.camera.Camera(pygame.camera.list_cameras()[0], (913, 512))
+                camera.start()
 
-        screen.blit(loadscreen,(0,0))
-        pygame.display.flip()
-
-        cam = pygame.camera.Camera(pygame.camera.list_cameras()[0], (1280, 720))
-        imagem = cam.get_image()
-
-        #cv2.imwrite(os.path.join("temp", filename), imagem)
-        pygame.image.save(cam, os.path.join("temp", filename))
-        cam.set(cv2.CAP_PROP_FRAME_WIDTH, 913)
-        cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 512)
-        imagem, show_image = editImage(filename)
-        screen.blit(imagem, (0,0))
-        pygame.display.flip()
-
-        while show_image == True:
-            if checkClick() == "space":
-                frame = cam.get_image()
-                frameProcess(frame)
-                show_image = False
-
-            elif checkClick() == "esc":
-                cam.release()
+                
+                imagem, edited = editImage(imagem = os.path.join("temp", filename), filename = filename)
+                imagem = pygame.transform.scale(imagem, screen_res)
+                screen.blit(imagem, (0,0))
+                pygame.display.flip()
+                while edited:
+                    capture = camera.get_image()
+                    for event in pygame.event.get(): # checa se acontecer o evento 
+                        if event.type == pygame.KEYDOWN: # se apressionar espaço 
+                             if event.key == pygame.K_SPACE:
+                                    
+                                    scaled_capture = pygame.transform.scale(capture, screen_res)
+                                    screen.blit(scaled_capture, (0,0))
+                                    screen.blit(cam_overlay, (0,0))
+                                    edited = False
+                                    
+                
+            elif event.key == pygame.K_ESCAPE: #se pressionar esc
+                camera.stop()
                 pygame.quit()
                 sys.exit()
 
-    elif checkClick() == "esc":
-        cam.release()
-        pygame.quit()
-        sys.exit()
