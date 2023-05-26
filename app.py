f9819bce716f71522f3bbacf362016c75e531399
berndof <128995129+berndof@users.noreply.github.com>
2023-05-26 14:38:44 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,85 +9,104 @@ import sys
 pygame.init()
 pygame.camera.init()
 
+class Capture():
+    def __init__(self):
 
-camera = pygame.camera.Camera(pygame.camera.list_cameras()[0], (800, 600))
-camera2 = pygame.camera.Camera(pygame.camera.list_cameras()[0], (1280, 720))
+        self.clock = pygame.time.Clock()
+        self.rate = 30
 
+        self.camera_res = (913, 512)
+        self.camera_full_res = (1280, 720)
 
-#Define a resolução da camera
-camera_res = (800, 600)
-camera_full_res = (1280, 720)
-    
-screen = pygame.display.set_mode((1280,720), pygame.FULLSCREEN) # Inicia uma janela em tela cheia
-screen_res = screen.get_size() # Pega a resolução da janela = resolução máxima do monitor
+        self.camera = pygame.camera.Camera(pygame.camera.list_cameras()[0], self.camera_res)
+        self.camera2 = pygame.camera.Camera(pygame.camera.list_cameras()[0], self.camera_full_res)
+        
+        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN) # Inicia uma janela em tela cheia
+        self.screen_res = self.screen.get_size() # Pega a resolução da janela = resolução máxima do monitor
 
-cam_overlay = pygame.image.load("assets//overlay.png").convert_alpha() # carrega o overlay da pasta overlay
-cam_overlay = pygame.transform.scale(cam_overlay, screen_res)# redimennsiona para o tamanho da tela
+        self.cam_overlay = pygame.image.load("assets//overlay.png").convert_alpha() # carrega o overlay da pasta overlay
+        self.cam_overlay = pygame.transform.scale(self.cam_overlay, self.screen_res)# redimennsiona para o tamanho da tela
 
-loadscreen = pygame.image.load("assets//loadscreen.png").convert() # carrega o overlay da pasta overlay
-loadscreen = pygame.transform.scale(loadscreen, screen_res)
+        self.loadscreen = pygame.image.load("assets//loadscreen.png").convert() # carrega o overlay da pasta overlay
+        self.loadscreen = pygame.transform.scale(self.loadscreen, self.screen_res)
 
-overlay = pygame.image.load("assets//overlay2.png").convert_alpha() # carrega o overlay da pasta overlay
-overlay = pygame.transform.scale(overlay, camera_full_res)
+        self.overlay = pygame.image.load("assets//overlay2.png").convert_alpha() # carrega o overlay da pasta overlay
+        self.overlay = pygame.transform.scale(self.overlay, self.camera_full_res)
 
-while True:
-    capture = camera.get_image()
-    scaled_capture = pygame.transform.scale(capture, screen_res)
-    screen.blit(scaled_capture, (0,0))
-    screen.blit(cam_overlay, (0,0))
-    pygame.display.flip()
-    for event in pygame.event.get(): 
-        if event.type == pygame.KEYDOWN:
-            camera.stop() 
-            camera2.start()
+        self.snapshot = pygame.surface.Surface(self.screen_res, 0, self.screen)
+        self.picture = pygame.surface.Surface(self.screen_res, 0, self.screen)
 
-            if event.key == pygame.K_SPACE:
-                start_time = pygame.time.get_ticks()
-                # Define o tempo da animação
-                animation_time = 150
-                while pygame.time.get_ticks() - start_time < animation_time:
-                    # Alterna entre preto e branco
-                    if pygame.time.get_ticks() % 100 < 50:
-                        screen.fill((255, 255, 255))
-                        pygame.display.flip()
-                    else:
-                        screen.fill((0, 0, 0))
-                        pygame.display.flip()
+        self.animationTime = 250 #ms
 
-                time.sleep(2)
-                ####AQUI BURRO! 
-                capture = camera2.get_image()
-                screen.blit(loadscreen, (0,0))
+    def close(self):
+        self.camera.stop()
+        self.camera2.stop()
+        pygame.quit()
+        sys.exit()
+
+    def takePicture(self):
+        self.connectCam2()
+        self.filename = datetime.datetime.now().strftime("%d-%m-%Y_%H-%M-%S Massa-festa-pinhao.png" )
+        self.picture = self.camera2.get_image()
+        self.picture.blit(self.overlay, (0,0))
+        pygame.image.save(self.picture, os.path.join("Fotos Massa", self.filename))   
+        self.screen.blit(pygame.transform.scale(self.picture, self.screen_res), (0,0))
+        pygame.display.flip()
+        self.taked = True
+        self.connectCam1()
+        while self.taked == True:
+            for event in pygame.event.get(): 
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        self.video()
+                    elif event.key == pygame.K_ESCAPE:
+                        self.close()
+
+    def animate(self):
+        start_time = pygame.time.get_ticks()
+        while pygame.time.get_ticks() - start_time < self.animationTime:
+            if pygame.time.get_ticks() % 100 < 50:
+                self.screen.fill((255, 255, 255))
                 pygame.display.flip()
-
-                filename = datetime.datetime.now().strftime("%d-%m-%Y_%H-%M-%S Massa-festa-pinhao.png" )
-                screen.blit(capture, (0,0))
-                screen.blit(overlay, (0,0))
+            else:
+                self.screen.fill((0, 0, 0))
                 pygame.display.flip()
-                camera2.stop()
-                pygame.image.save(screen, os.path.join("Fotos Massa", filename))
-                #screen = pygame.transform.scale(screen, screen_res)
-                #screen.blit(screen, (0,0))
-                #camera2.stop()
-                try:
-                    camera.start()  # Tenta iniciar a camera
-                except pygame.error:
-                    print("Failed to connect")
-                    # Se não startar, vai pra proxima camera da lista
-                    continue
-                print(f"Connected to camera ")
-                while True :
-                    capture = camera.get_image()
-                    for event in pygame.event.get(): # checa se acontecer o evento 
-                        if event.type == pygame.KEYDOWN: # se apressionar espaço 
-                             if event.key == pygame.K_SPACE: 
-                                scaled_capture = pygame.transform.scale(capture, screen_res)
-                                screen.blit(scaled_capture, (0,0))
-                                screen.blit(cam_overlay, (0,0))
-                                pygame.display.flip()
-                                break
-            elif event.key == pygame.K_ESCAPE: #se pressionar esc
-                camera.stop()
-                pygame.quit()
-                sys.exit()
+                
+        self.screen.blit(self.loadscreen, (0,0))
+        pygame.display.flip()
+        time.sleep(1.4) 
 
+    def connectCam1(self):
+        self.camera2.stop()
+        self.camera.start()
+
+    def connectCam2(self):
+        self.camera.stop()
+        self.camera2.start()
+        self.animate()
+
+    def video(self):
+            self.connectCam1()
+
+            while True:
+        
+                self.snapshot = pygame.transform.scale(self.camera.get_image(), self.screen_res)
+                self.snapshot.blit(pygame.transform.scale(self.cam_overlay, self.screen_res), (0,0))
+        
+                self.screen.blit(self.snapshot, (0,0))
+        
+                for event in pygame.event.get(): 
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_SPACE:
+                            self.takePicture()
+                        elif event.key == pygame.K_ESCAPE:
+                            self.close()
+
+                pygame.display.flip()
+                self.clock.tick(self.rate)
+
+
+if __name__ == "__main__":
+
+    capture = Capture()  # Cria uma instância da classe Capture
+    capture.video()
